a65481d272ce21b213b07465782f0500a7ff76d8
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-08-25 05:59:18 +0000

    rails g controller, model

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,7 @@
+class RecordsController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+end
---
+++ db/migrate/20220825055706_create_records.rb
@@ -0,0 +1,11 @@
+class CreateRecords < ActiveRecord::Migration[6.1]
+  def change
+    create_table :records do |t|
+
+      t.string :title
+      t.text :content
+      t.datetime :start_time
+      t.timestamps
+    end
+  end
+end
