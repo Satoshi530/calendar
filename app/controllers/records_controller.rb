bca8869afc1747a4d83b5dafdc88491c7d609424
EC2 Default User <ec2-user@ip-172-31-9-166.ec2.internal>
2022-08-28 13:56:58 +0000

    カレンダー装飾

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,7 +1,46 @@
 class RecordsController < ApplicationController
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      flash[:notice] = "記録しました"
+      redirect_to records_path
+    end
+  end
+
   def show
+    @record = Record.find(params[:id])
   end
 
   def index
+    @records = Record.all
+
+  end
+
+  def edit
+    @record = Record.find(params[:id])
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      flash[:notice] = "更新しました"
+      redirect_to record_path(@record.id)
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+    redirect_to records_path
+  end
+
+  private
+  def record_params
+    params.require(:record).permit(:title, :content, :start_time)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :records
-  root to: 'records/index'
+  root to: "records#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
